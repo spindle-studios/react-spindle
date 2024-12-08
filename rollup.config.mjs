03c383460709e0bbb296e35854e3aa2efd90100f
olafkotur <olafkotur@icloud.com>
2024-12-08 21:08:56 +0000

    Rollup check

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,13 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import fs from 'fs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
+
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+const { dependencies, peerDependencies, devDependencies } = packageJson;
 
 export default {
   input: 'src/index.ts',
@@ -11,7 +15,8 @@ export default {
     dir: 'dist',
     format: 'esm',
     sourcemap: true,
-    chunkFileNames: '[name]-[hash].js',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
   },
   plugins: [
     peerDepsExternal(),
@@ -24,5 +29,9 @@ export default {
     }),
     terser(),
   ],
-  external: ['react', 'react-dom', 'tailwindcss', 'tailwindcss-animate'],
+  external: [
+    ...Object.keys(dependencies || {}),
+    ...Object.keys(peerDependencies || {}),
+    ...Object.keys(devDependencies || {}),
+  ],
 };
