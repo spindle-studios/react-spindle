e992d569c813c8819a21a4b1a03ce6c76396cf3d
olafkotur <olafkotur@icloud.com>
2024-12-09 19:54:38 +0000

    Potentially fixed some styling issues for consumer

--- src/stories/Loader.stories.tsx
+++ src/stories/Loader.stories.tsx
@@ -3,48 +3,18 @@ import { Loader } from '@components/Loader';
 import React from 'react';
 
 export const Default = () => (
-  <div className="flex flex-col gap-4">
-    <div className="flex flex-row gap-2 items-center">
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="sm" variant="spinner" />
-      </Card>
+  <div className="flex flex-row gap-2 items-center">
+    <Card className="w-[100px] aspect-square justify-center items-center flex">
+      <Loader size="sm" />
+    </Card>
 
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="sm" variant="dots" />
-      </Card>
+    <Card className="w-[100px] aspect-square justify-center items-center flex">
+      <Loader size="md" />
+    </Card>
 
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="sm" variant="ring" />
-      </Card>
-    </div>
-
-    <div className="flex flex-row gap-2 items-center">
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="md" variant="spinner" />
-      </Card>
-
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="md" variant="dots" />
-      </Card>
-
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="md" variant="ring" />
-      </Card>
-    </div>
-
-    <div className="flex flex-row gap-2 items-center">
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="lg" variant="spinner" />
-      </Card>
-
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="lg" variant="dots" />
-      </Card>
-
-      <Card className="w-[100px] aspect-square justify-center items-center flex">
-        <Loader size="lg" variant="ring" />
-      </Card>
-    </div>
+    <Card className="w-[100px] aspect-square justify-center items-center flex">
+      <Loader size="lg" variant="secondary" />
+    </Card>
   </div>
 );
 
